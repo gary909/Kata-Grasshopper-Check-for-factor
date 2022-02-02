31a6c74e8d822e95cb801530e6407a0b5729b570
gary <gary.white@lodgit.com>
2022-02-02 12:57:25 +0100

    code

---
+++ index.js
@@ -0,0 +1,13 @@
+function checkForFactor (base, factor) {
+    // code here
+}
+
+console.log(checkForFactor(10,2)); // true
+console.log(checkForFactor(63,7)); // true
+console.log(checkForFactor(2450,5)); // true
+console.log(checkForFactor(24612,3)); // true
+
+console.log(checkForFactor(9,2)); // false
+console.log(checkForFactor(63,7)); // false
+console.log(checkForFactor(2453,5)); // false
+console.log(checkForFactor(24617,3)); // false
